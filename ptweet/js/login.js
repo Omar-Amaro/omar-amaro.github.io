cc6efb83c5eb1d959d68a056288f8e55178d7341
Omar-Amaro <omar9939@hotmail.com>
2021-11-08 16:23:00 -0700

    Arrays, objects y Local Storage

--- ptweet/js/login.js
+++ ptweet/js/login.js
@@ -1,12 +1,31 @@
 window.onload = function (){
     var boton= document.getElementById("btnaceptar");
-    var txtusuario = document.getElementById("txtusuario");
+    var txtUsuario = document.getElementById("txtusuario");
     var txtPassword = document.getElementById("txtPassword");
     boton.addEventListener('click', function(){
         let usuario = txtUsuario.value;
         let pass= txtPassword.value;
-        if(usuario.trim() == "admin" && pass.trim() == "123"){
-            alert("Listo")
+        let arregloUsuarios=[
+            { id:1, nombre:"Ludoviquito Pluche", imagen: "ludo.jpg",username: "ludop", password: "123"},
+            { id:1, nombre:"Jr Pluche", imagen: "jr.jpg",username: "Jrp", password: "123"},
+            { id:1, nombre:"Federuica Pluche", imagen: "fede.jpg",username: "Fedep", password: "123"}
+        ];
+        let encontro =false;
+        
+        arregloUsuarios.forEach(fila=>{
+            if(fila.username = usuario.trim() && fila.password==pass.trim()){
+                encontro=true;
+                localStorage.setItem("usuario",JSON.stringify(fila));
+                let fecha=new Date();
+                localStorage.setItem("fecha",fecha.getFullYear()+"/"+fecha.getMonth()+"/"+fecha.getDay()+" " + fecha.getHours()+":"+fecha.getMinutes());
+            }
+        });
+        
+        if(encontro){
+            ///Redireccionar
+            location.href="./index.html";
+            
+
         }else {
             document.getElementById("error").innerHTML="Credenciales Incorrectas";
             txtUsuario.style.backgroundColor="#FF0000";
