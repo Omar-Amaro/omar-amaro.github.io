e96df03fc516afa0d1b49072611dd85360bfb7cb
Omar-Amaro <omar9939@hotmail.com>
2021-11-12 11:57:48 -0700

    PracticaJSON

--- ptweet/js/index.js
+++ ptweet/js/index.js
@@ -1,7 +1,19 @@
 window.onload=()=>{
     let usuario=JSON.parse(localStorage.getItem("usuario"));
-    document.getElementById("imguser").attributes.src="./imagenes/"+usuario.img
+    document.getElementById("imgUser").attributes.src="./imagenes/"+usuario.img;
     let arrayTw=[];
+    var xobj= new XMLHttpRequest();
+    xobj.overrideMimeType("application/jason");
+    xobj.open('GET','./js/tweets.json',true);
+    xobj.onreadystatechange=function(){
+        if(xobj.readyState == 4 && xobj.status =='200'){
+            arrayTw=JSON.parse(xobj.responseText);
+            console.log(arrayTw);
+            crearPost();
+        }
+    }
+    xobj.send(null);
+    
     //console.log(usuario);
     //document.getElementById("usuario").innerHTML="Hola "+ usuario.nombre;
     let txtTweet = document.getElementById("txtTweet");
@@ -25,26 +37,58 @@ window.onload=()=>{
 
     });
 
-    function crearPost(){
-        console.log(arrayTw);
-        var todo="";
-        arrayTw.forEach(el=>{
-            todo+=`<div class="post">
+    function crearPost() {
+        var todo = "";
+        arrayTw.forEach(tweet => {
+            comentarios = "";
+            tweet.comentarios.forEach(comment => {
+                comentarios += `<div class="comment">
             <div>
-                <img src="Imagenes/${el.img}" alt="" class="imgUser">
+                <img src="img/${comment.img}" alt="" class="imgUser">
             </div>
             <div>
                 <h2> 
-                    <span>${el.nombre}</span>
-                    <span>${el.username}</span>
+                    <span>${comment.nombre}</span>
+                    <span>@${comment.username}</span>
                 </h2>
                 <div class="textTweet">
-                    ${el.mensaje}
+                    ${comment.comentarios}
+                </div>
+            </div>
+        </div>`;
+            });
+            todo += `<div class="post">
+            <div>
+                <img src="img/${tweet.img}" alt="" class="imgUser">
+            </div>
+            <div>
+                <h2> 
+                    <span>${tweet.nombre}</span>
+                    <span>@${tweet.username}</span>
+                </h2>
+                <div class="textTweet">
+                    ${tweet.mensaje}
+                </div>
+                <div class="likeRetweet">
+                    <h2>
+                        <span>
+                            <i class="far fa-heart"></i>
+                            ${tweet.likes}
+                        </span>
+                    </h2>
+                    <h2>
+                        <span>
+                            <i class="fas fa-share"></i>
+                            ${tweet.retweets}
+                        </span>
+                    </h2>
+                </div>
+                <div class="comments">
+                    ${comentarios}
                 </div>
             </div>
         </div>`;
         });
-        document.getElementById("posts").innerHTML=todo;
-
+        document.getElementById("posts").innerHTML = todo;
     }
 };
