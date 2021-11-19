cbe10c36157d807b2e3fcfcaeb7e883bb6806fe0
Omar-Amaro <omar9939@hotmail.com>
2021-11-19 01:17:08 -0700

    Atributos Dinámicos y Evaluación Unidad 3 (Corrección de todo XD)

--- elements/js/script.js
+++ elements/js/script.js
@@ -1,86 +1,49 @@
 window.onload = () => {
-    var arrayElements = [];
-    let txtElemento = document.getElementById('txtElemento');
-    let txtSimbolo = document.getElementById('txtSimbolo');
-    let txtDescripcion = document.getElementById('txtDescr');
-    let xobj = new XMLHttpRequest();
-    let periodic = document.getElementsByClassName("periodic-table");
-    let dataDiv = document.getElementById('data');
-    document.body.addEventListener('click', () => {
-        dataDiv.style.display = 'none';
-    }, true);
-    xobj.overrideMimeType('application/json');
-    xobj.open('GET', './js/PeriodicTableJSON.json', true);
-    xobj.onreadystatechange = () => {
-        if (xobj.readyState = 4 && xobj.status == 200) {
-            let response = JSON.parse(xobj.responseText);
-            arrayElements = response.elements;
-            let todo = "";
-            arrayElements.forEach(element => {
-                let elementClass = "";
-                let elementId = "";
-                switch (element.category) {
-                    case 'metalloid':
-                        elementClass = 'metalloid';
-                        break;
-                    case 'noble gas':
-                        elementClass = 'noble-gas';
-                        break;
-                    case 'alkali metal':
-                        elementClass = 'alkali';
-                        break;
-                    case 'alkaline earth metal':
-                        elementClass = 'alkaline';
-                        break;
-                    case 'lanthanide':
-                        elementClass = 'lanthanide';
-                        break;
-                    case 'actinide':
-                        elementClass = 'actinide';
-                        break;
-                    case 'polyatomic nonmetal':
-                        elementClass = 'polyatomic-non-metal';
-                        break;
-                    case 'diatomic nonmetal':
-                        elementClass = 'diatomic-nonmetal';
-                        break;
-                    case 'transition metal':
-                    case 'post-transition metal':
-                        break;
-                    default:
-                        elementClass = 'unknown';
-                        break;
-                }
-                switch (element.name) {
-                    case 'Hydrogen':
-                        elementClass = 'hydrogen';
-                        break;
-                    case 'Helium':
-                    case 'Boron':
-                    case 'Aluminium':
-                    case 'Cerium':
-                    case 'Thorium':
-                        elementId = element.name.toLowerCase();
-                        break;
-                }
-                todo += `<li id="${elementId}" class="${elementClass}" data-id="${element.number}" data-sym="${element.symbol}" data-name="${element.name}" data-desc="${element.summary}">
-                    <abbr title="${element.name}">${element.symbol}</abbr>
+  //Cargar JSON
+
+  let arrayElem = [];
+  var xobj = new XMLHttpRequest();
+  xobj.overrideMimeType("application/jason");
+  xobj.open("GET", "./js/PeriodicTableJSON.json", true);
+  xobj.onreadystatechange = function () {
+    if (xobj.readyState == 4 && xobj.status == "200") {
+      let json = JSON.parse(xobj.responseText);
+      arrayElem = json.elements;
+      let todo = "";
+      arrayElem.forEach((element) => {
+        if (element.name == "Hydrogen") {
+          todo += `<li class="hydrogen" data-id="${element.number}" data-name="${element.name}" data-sim="${element.symbol}"  data-descr="${element.summary}" >
+                    <abbr title="Hydrogen">H</abbr>
                 </li>`;
-            });
-            periodic[0].innerHTML = todo;
-            let elems = document.getElementsByTagName('li');
-            for (let i = 0; i < elems.length; i++) {
-                elems[i].addEventListener('click', (e) => {
-                    dataDiv.style.display = 'block';
-                    let name = elems[i].dataset.name;
-                    let sym = elems[i].dataset.sym;
-                    let desc = elems[i].dataset.desc;
-                    txtElemento.innerHTML = name;
-                    txtSimbolo.innerHTML = sym;
-                    txtDescripcion.innerHTML = desc;
-                });
+        } else {
+            if(element.category.startsWith("unknown")){
+                todo+=`<li class="unknown" data-id="${element.number}" data-name="${element.name}" data-sim="${element.symbol}"  data-descr="${element.summary}" >
+                    <abbr title="${element.name} ">${element.symbol}</abbr>
+                </li>`;
+            }else{
+                todo+=`<li data-id="${element.number}" class="${element.category.replace(" ","-").replace(" ","")}" data-name="${element.name}" data-sim="${element.symbol}"  data-descr="${element.summary}" >
+                <abbr title="${element.name} ">${element.symbol}</abbr>
+            </li>`;
             }
+
         }
-    };
-    xobj.send(null);
-}
+      });
+      document.getElementsByClassName("periodic-table")[0].innerHTML = todo;
+
+      let elem = document.getElementsByTagName("li");
+      console.log(elem.length);
+      for (let x = 0; x < elem.length; x++) {
+        elem[x].addEventListener("click", (e) => {
+          let nom = elem[x].getAttribute("data-name");
+          let sim = elem[x].getAttribute("data-sim");
+          let des = elem[x].getAttribute("data-descr");
+          document.getElementById("txtElemento").innerText = nom;
+          document.getElementById("txtSimbolo").innerText = sim;
+          document.getElementById("txtDescr").innerText = des;
+        });
+      }
+    }
+  };
+  xobj.send(null);
+  //CREAR EVENTOS
+};
